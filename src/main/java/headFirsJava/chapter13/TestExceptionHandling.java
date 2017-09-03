cad0102bc96a00f0d6948150080e4be733f2bf80
Vladyslove <od.vladyslav@gmail.com>
2017-09-03 23:22:27 +0300

    Some changes in TestExceptionHandling class

---
+++ src/main/java/headFirsJava/chapter13/TestExceptionHandling.java
@@ -0,0 +1,31 @@
+package headFirsJava.chapter13;
+
+/**
+ * Created by User on 03.09.2017.
+ */
+public class TestExceptionHandling {
+
+    private boolean abandonAllHope;
+
+    public void takeRisk() throws BadException {
+        if (abandonAllHope) {
+            throw new BadException();
+        }
+    }
+
+    static TestExceptionHandling anObject = new TestExceptionHandling();
+
+    public static void croosFingers() {
+        try {
+            anObject.takeRisk();
+        } catch (BadException ex) {
+            System.out.println("Aaaaaa");
+            ex.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+       croosFingers();
+    }
+}
