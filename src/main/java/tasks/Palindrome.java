5f17a3e3112fcec902be1751a9e87c8658359fe4
Vladyslove <od.vladyslav@gmail.com>
2017-09-03 20:03:16 +0300

    added some tests for Palindrome Task

---
+++ src/main/java/tasks/Palindrome.java
@@ -0,0 +1,7 @@
+package tasks.ShortTasks;
+
+/**
+ * Created by User on 03.09.2017.
+ */
+public class Palindrome {
+}
---
+++ src/test/java/PalindromeTests.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by User on 30.08.2017.
+ */
+public class PalindromeTests {
+}
