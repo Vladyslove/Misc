9fe0cd99e7fccd9c8c213d722968e080030c88f8
Vladyslove <od.vladyslav@gmail.com>
2017-09-03 20:51:22 +0300

    commit just for training

--- src/test/java/PalindromeTests.java
+++ src/test/java/PalindromeTests.java
@@ -1,7 +1,62 @@
-package PACKAGE_NAME;
+import org.junit.Test;
 
-/**
- * Created by User on 30.08.2017.
- */
+import static tasks.palindrome.Palindrome.isPalindrome;
+import static tasks.palindrome.Palindrome.isSimpleNumber;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
 public class PalindromeTests {
+
+    @Test
+    public void test_isSimpleNumber() {
+
+        assertThat((isSimpleNumber(13)), is(true));
+        assertThat((isSimpleNumber(11)), is(true));
+        assertThat((isSimpleNumber(17)), is(true));
+        assertThat((isSimpleNumber(3)), is(true));
+    }
+
+    @Test
+    public void test_isNotSimpleNumber() {
+
+        assertThat((isSimpleNumber(15_452)), is(false));
+        assertThat((isSimpleNumber(150_000)), is(false));
+        assertThat((isSimpleNumber(17_245)), is(false));
+        assertThat((isSimpleNumber(99)), is(false));
+    }
+
+    @Test
+    public void test_isPalindrome() {
+
+        assertThat(isPalindrome("ABBA"), is(true));
+        assertThat(isPalindrome("12321"), is(true));
+        assertThat(isPalindrome("racecar"), is(true));
+    }
+
+    @Test
+    public void test_isNotPalindrome() {
+
+        assertThat(isPalindrome("12345"), is(false));
+        assertThat(isPalindrome("palindrome"), is(false));
+        assertThat(isPalindrome("simple"), is(false));
+    }
+
+/*
+    @Ignore
+    @Test
+    public void test_maxOfPalindromes() {
+
+        List listOfPalindromes = new ArrayList();
+
+        listOfPalindromes.add(111);
+        listOfPalindromes.add(353);
+        listOfPalindromes.add(959);
+        listOfPalindromes.add(252);
+
+        assertThat(maxOfPalindromes(), is(959));
+
+    }
+*/
+
 }
