ed06c62029ba9655029e334536dd3ef2dc22b8f3
Vladyslove <od.vladyslav@gmail.com>
2017-09-04 01:00:06 +0300

    Another manipulation with Exceptions - stack of catch block in order of priority tailored with classes inheritance. Also added some changes in .gitignore

---
+++ src/main/java/headFirsJava/chapter13/multipleExceptions/MulptipleExceptionsHandling.java
@@ -0,0 +1,34 @@
+package headFirsJava.chapter13.multipleExceptions;
+
+/**
+ * Created by User on 04.09.2017.
+ */
+public class MulptipleExceptionsHandling {
+}
+
+    class Laundry{
+        public void doLaundry() throws LingerieException, ClothingException, TeeShirtException{
+
+        }
+    public static void main(String[] args) {
+        Laundry laundry = new Laundry();
+        try {
+            laundry.doLaundry();
+        } catch (LingerieException e) {
+            e.printStackTrace();
+        } catch (TeeShirtException e) {
+            e.printStackTrace();
+        } catch (ClothingException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+    class ClothingException extends Exception {
+    }
+
+    class TeeShirtException extends ClothingException {
+    }
+
+     class LingerieException extends TeeShirtException {
+    }
