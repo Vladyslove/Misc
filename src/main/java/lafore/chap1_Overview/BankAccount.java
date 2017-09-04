aa1d8d5f8fab20d3132f46119535a114c4089f14
Vladyslove <od.vladyslav@gmail.com>
2017-09-04 15:48:32 +0300

    Beginning of improving skills of data Structures and algorithms in Java with Robert Lafore. Added first simple app

---
+++ src/main/java/lafore/chap1_Overview/BankAccount.java
@@ -0,0 +1,38 @@
+package lafore.chap1_Overview;
+
+/**
+ * Created by User on 04.09.2017.
+ */
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double openingBalance) {
+        balance = openingBalance;
+    }
+
+    public void deposit (double amount) {
+        balance = balance + amount;
+    }
+
+    public void withdraw(double amount) {
+        balance = balance - amount;
+    }
+
+    public void display() {
+        System.out.println("balance = " + balance);
+    }
+
+}
+
+class BankApp {
+
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount(99.99);
+
+        bankAccount.deposit(11.11);
+        bankAccount.withdraw(22.22);
+        System.out.println("After transactionsf");
+        bankAccount.display();
+    }
+
+}
