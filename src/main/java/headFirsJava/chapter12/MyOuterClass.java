90288ba89170ee47f0ae86a6050019419284d6fd
Vladyslove <od.vladyslav@gmail.com>
2017-09-11 00:46:24 +0300

    Start of studying new gist 'inner class'

---
+++ src/main/java/headFirsJava/chapter12/MyOuterClass.java
@@ -0,0 +1,12 @@
+package headFirsJava.chapter12;
+
+/**
+ * Created by User on 11.09.2017.
+ */
+public class MyOuterClass {
+    class MyInnerClass {
+        void go() {
+            // inner class in fully enclosed by outer class
+        }
+    }
+}
