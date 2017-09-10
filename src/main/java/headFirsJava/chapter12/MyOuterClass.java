de5357d403b39b2488d0ead78053d6f2028e081b
Vladyslove <od.vladyslav@gmail.com>
2017-09-11 00:51:10 +0300

    considering inner class attributes

--- src/main/java/headFirsJava/chapter12/MyOuterClass.java
+++ src/main/java/headFirsJava/chapter12/MyOuterClass.java
@@ -4,9 +4,11 @@ package headFirsJava.chapter12;
  * Created by User on 11.09.2017.
  */
 public class MyOuterClass {
+    private int x;
     class MyInnerClass {
         void go() {
+            x = 42; // use 'x' as if it were a variable of the inner class
             // inner class in fully enclosed by outer class
         }
-    }
-}
+    } //close inner class
+} // close outer class
