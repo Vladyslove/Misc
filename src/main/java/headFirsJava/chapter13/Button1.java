e542fb4d726718d29405e6ccc9622ac2c3da9e21
Vladyslove <od.vladyslav@gmail.com>
2017-09-13 02:15:48 +0300

    Added another variant of the region specifying

--- src/main/java/headFirsJava/chapter13/Button1.java
+++ src/main/java/headFirsJava/chapter13/Button1.java
@@ -9,7 +9,8 @@ import java.awt.*;
 public class Button1 {
     public static void main(String[] args) {
         Button1 gui = new Button1();
-        gui.go();
+//        gui.go();
+        gui.go1();
     }
 
     private void go() {
@@ -17,6 +18,14 @@ public class Button1 {
         JButton button = new JButton("click like you mean it");
         frame.getContentPane().add(BorderLayout.EAST, button);
         frame.setSize(250,250);
+        frame.setVisible(true);
+    }
+
+    private void go1() {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("There is no spoon...");
+        frame.getContentPane().add(BorderLayout.NORTH, button);
+        frame.setSize(200,200);
         frame.setVisible(true);
     }
 }
