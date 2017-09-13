53e18afb76f4332a9e9e27d4325894e3530ec0fc
Vladyslove <od.vladyslav@gmail.com>
2017-09-13 15:11:24 +0300

    For working with JPanel added new class

---
+++ src/main/java/headFirsJava/chapter13/Panel.java
@@ -0,0 +1,15 @@
+package headFirsJava.chapter13;
+
+/**
+ * Created by User on 13.09.2017.
+ */
+public class Panel {
+    public static void main(String[] args) {
+        Panel gui = new Panel();
+        gui.go();
+    }
+
+    private void go() {
+    }
+
+}
