542286b1998b6006c1807e311e852a519b912730
Vladyslove <od.vladyslav@gmail.com>
2017-09-13 15:06:48 +0300

    Whole frame are layouted by regions

--- src/main/java/headFirsJava/chapter13/Button1.java
+++ src/main/java/headFirsJava/chapter13/Button1.java
@@ -10,14 +10,25 @@ public class Button1 {
     public static void main(String[] args) {
         Button1 gui = new Button1();
 //        gui.go();
-        gui.go1();
+//        gui.go1();
+        gui.go2();
     }
 
-    private void go() {
+    private void go2() {
         JFrame frame = new JFrame();
-        JButton button = new JButton("click like you mean it");
-        frame.getContentPane().add(BorderLayout.EAST, button);
-        frame.setSize(250,250);
+        JButton east = new JButton("East");
+        JButton west = new JButton("West");
+        JButton north = new JButton("North");
+        JButton south = new JButton("South");
+        JButton center = new JButton("Center");
+
+        frame.getContentPane().add(BorderLayout.EAST, east);
+        frame.getContentPane().add(BorderLayout.WEST, west);
+        frame.getContentPane().add(BorderLayout.NORTH, north);
+        frame.getContentPane().add(BorderLayout.SOUTH, south);
+        frame.getContentPane().add(BorderLayout.CENTER, center);
+
+        frame.setSize(300,300);
         frame.setVisible(true);
     }
 
@@ -30,4 +41,14 @@ public class Button1 {
         frame.setSize(200,200);
         frame.setVisible(true);
     }
+
+    private void go() {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("click like you mean it");
+        frame.getContentPane().add(BorderLayout.EAST, button);
+        frame.setSize(250,250);
+        frame.setVisible(true);
+    }
+
+
 }
