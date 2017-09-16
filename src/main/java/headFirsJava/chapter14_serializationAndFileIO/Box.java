248f036b95a3f4b272f5500086cd725da942c5df
Vladyslove <od.vladyslav@gmail.com>
2017-09-16 14:38:56 +0300

    It(implementation of Serialzable) says JVM to serialize object of this type

--- src/main/java/headFirsJava/chapter14_serializationAndFileIO/Box.java
+++ src/main/java/headFirsJava/chapter14_serializationAndFileIO/Box.java
@@ -1,7 +1,10 @@
 package headFirsJava.chapter14_serializationAndFileIO;
 
+import java.io.Serializable;
+
 /**
  * Created by User on 16.09.2017.
  */
-public class Box {
+public class Box implements Serializable {
+
 }
