c5cdb4ce7e7c14e20da15bcafd55761b93198fb2
Vladyslove <od.vladyslav@gmail.com>
2017-09-16 14:47:51 +0300

    Realised: connection to file named foo.ser if it exists. If it doesn't, make a new file. Made an ObjectOutputStreamchained to the connection stream. Now I'am going to tell it to write the object

--- src/main/java/headFirsJava/chapter14_serializationAndFileIO/Box.java
+++ src/main/java/headFirsJava/chapter14_serializationAndFileIO/Box.java
@@ -1,13 +1,35 @@
 package headFirsJava.chapter14_serializationAndFileIO;
 
-import java.io.Serializable;
+import java.io.*;
 
 /**
  * Created by User on 16.09.2017.
  */
 public class Box implements Serializable {
-    
+
     private int width;
     private int height;
 
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public static void main(String[] args) {
+        Box myBox = new Box();
+        myBox.setWidth(50);
+        myBox.setHeight(20);
+
+        try {
+            FileOutputStream fs = new FileOutputStream("src/main/resources/headFirstJava/chapter14/foo.ser");
+            ObjectOutputStream os = new ObjectOutputStream(fs);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
