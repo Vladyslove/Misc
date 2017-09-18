5276e39ca8bc81034562a1460656a68dbe0c5d65
Vladyslove <od.vladyslav@gmail.com>
2017-09-18 22:11:34 +0300

    Pond objects can be serialized; Class Pond has one istance variable, a Duck

---
+++ src/main/java/headFirsJava/chapter14_serializationAndFileIO/serializeAllOrNothing/Pond.java
@@ -0,0 +1,11 @@
+package headFirsJava.chapter14_serializationAndFileIO.serializeAllOrNothing;
+
+import java.io.Serializable;
+
+/**
+ * Created by User on 18.09.2017.
+ */
+public class Pond implements Serializable {
+
+    private Duck duck = new Duck();
+}
