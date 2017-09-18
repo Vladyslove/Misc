16cbe7ac40f8e55689dbad206587356054a2795c
Vladyslove <od.vladyslav@gmail.com>
2017-09-19 01:00:56 +0300

    Started text file example: e-Flashcards. Created class QuizCardBuilder. Firstly: create a skeleton of the class with explanation. Created method go, whick builds and displays the GUI.

---
+++ src/main/java/headFirsJava/chapter14_serializationAndFileIO/e_Flashcards/QuizCardBuilder.java
@@ -0,0 +1,11 @@
+package headFirsJava.chapter14_serializationAndFileIO.e_Flashcards;
+
+/**
+ * Created by User on 19.09.2017.
+ */
+public class QuizCardBuilder {
+
+    public void go() {
+
+    }
+}
