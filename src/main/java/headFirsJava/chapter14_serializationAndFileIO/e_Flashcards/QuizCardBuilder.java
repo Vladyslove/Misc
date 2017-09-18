1cdc58c84ebbf4bc112207d691a38b0b016987b4
Vladyslove <od.vladyslav@gmail.com>
2017-09-19 01:05:31 +0300

    This class triggered when user choosed 'Save' form from the File menu. Means the user wants to save all the cards in the current list as a 'set'

--- src/main/java/headFirsJava/chapter14_serializationAndFileIO/e_Flashcards/QuizCardBuilder.java
+++ src/main/java/headFirsJava/chapter14_serializationAndFileIO/e_Flashcards/QuizCardBuilder.java
@@ -1,5 +1,8 @@
 package headFirsJava.chapter14_serializationAndFileIO.e_Flashcards;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by User on 19.09.2017.
  */
@@ -7,5 +10,21 @@ public class QuizCardBuilder {
 
     public void go() {
 
+    }
+
+    private class NextCardListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class SaveMenuListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+        }
     }
 }
