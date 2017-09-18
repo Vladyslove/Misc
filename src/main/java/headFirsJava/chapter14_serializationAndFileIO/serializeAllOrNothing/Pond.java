509a6777ce75ae11787a2c11424b08f7419fb315
Vladyslove <od.vladyslav@gmail.com>
2017-09-18 22:17:15 +0300

    When we serialize myPond object, hence Duck is automatically gets serialized

--- src/main/java/headFirsJava/chapter14_serializationAndFileIO/serializeAllOrNothing/Pond.java
+++ src/main/java/headFirsJava/chapter14_serializationAndFileIO/serializeAllOrNothing/Pond.java
@@ -1,6 +1,6 @@
 package headFirsJava.chapter14_serializationAndFileIO.serializeAllOrNothing;
 
-import java.io.Serializable;
+import java.io.*;
 
 /**
  * Created by User on 18.09.2017.
@@ -8,4 +8,19 @@ import java.io.Serializable;
 public class Pond implements Serializable {
 
     private Duck duck = new Duck();
+
+    public static void main(String[] args) {
+        Pond myPond = new Pond();
+
+        try (FileOutputStream fs = new FileOutputStream("Pond.ser")) {
+            ObjectOutputStream os = new ObjectOutputStream(fs);
+            os.writeObject(myPond);
+            os.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
