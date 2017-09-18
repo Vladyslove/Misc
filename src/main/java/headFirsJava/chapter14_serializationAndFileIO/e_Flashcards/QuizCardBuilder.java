630a7054d347262e15cb6ef1f7c653653d015a8f
Vladyslove <od.vladyslav@gmail.com>
2017-09-19 01:10:16 +0300

    this method called by the SMListener; does the actual file writing

--- src/main/java/headFirsJava/chapter14_serializationAndFileIO/e_Flashcards/QuizCardBuilder.java
+++ src/main/java/headFirsJava/chapter14_serializationAndFileIO/e_Flashcards/QuizCardBuilder.java
@@ -2,6 +2,7 @@ package headFirsJava.chapter14_serializationAndFileIO.e_Flashcards;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 /**
  * Created by User on 19.09.2017.
@@ -27,12 +28,18 @@ public class QuizCardBuilder {
 
         }
 
-     private class NewMenuListener implements ActionListener {
+    }
+    private class NewMenuListener implements ActionListener {
 
          @Override
          public void actionPerformed(ActionEvent e) {
-             
+
          }
-     }
+
     }
+
+    private void saveFile(File file) {
+        
+    }
+
 }
