90e7b58b60e7f90a54e6408b02a8811311962e16
Vladyslove <od.vladyslav@gmail.com>
2017-09-19 14:44:46 +0300

    Continue making e_Flashcards. Outline of QuizCardPlayer Class

--- src/main/java/headFirsJava/chapter14_serializationAndFileIO/e_Flashcards/QuizCardBuilder.java
+++ src/main/java/headFirsJava/chapter14_serializationAndFileIO/e_Flashcards/QuizCardBuilder.java
@@ -24,6 +24,7 @@ public class QuizCardBuilder {
         builder.go();
     }
 
+
     public void go() {
         frame = new JFrame("Quiz Card Bulder");
         JPanel mainPanel = new JPanel();
---
+++ src/main/java/headFirsJava/chapter14_serializationAndFileIO/e_Flashcards/QuizCardPlayer.java
@@ -0,0 +1,38 @@
+package headFirsJava.chapter14_serializationAndFileIO.e_Flashcards;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+/**
+ * Created by User on 19.09.2017.
+ */
+public class QuizCardPlayer {
+    public void go() {
+
+    }
+
+    class NextCardListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    class OpenMenuListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private void loadFile (File file){
+
+    }
+
+    private void makeCard(String lineToParse) {
+
+    }
+}
