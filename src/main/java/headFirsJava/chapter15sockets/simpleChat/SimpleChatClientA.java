9d312ce9147c082284e7b857bd73b494f3e7ce00
Vladyslove <od.vladyslav@gmail.com>
2017-09-21 20:51:58 +0300

    Method void are made.

--- src/main/java/headFirsJava/chapter15sockets/simpleChat/SimpleChatClientA.java
+++ src/main/java/headFirsJava/chapter15sockets/simpleChat/SimpleChatClientA.java
@@ -1,6 +1,7 @@
 package headFirsJava.chapter15sockets.simpleChat;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.PrintWriter;
@@ -16,10 +17,20 @@ public class SimpleChatClientA {
     Socket sock;
 
     public void go() {
-
+        JFrame frame = new JFrame("Ludicrously Simple Chat Client");
+        JPanel mainPanel = new JPanel();
+        outgoing = new JTextField(20);
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
+        mainPanel.add(outgoing);
+        mainPanel.add(sendButton);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        setUpNetworking();
+        frame.setSize(400,500);
+        frame.setVisible(true);
     }
 
-    public void SetUpNetworking() {
+    public void setUpNetworking() {
 
     }
 
@@ -27,7 +38,6 @@ public class SimpleChatClientA {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-
         }
     }
 }
