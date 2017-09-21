a68673770c8c60ccc9169a17d2745d7b0c3bef26
Vladyslove <od.vladyslav@gmail.com>
2017-09-22 00:01:54 +0300

    Started another example for understanding of threads working. We'll check how two threads works together.

---
+++ src/main/java/headFirsJava/chapter15sockets/threads/RyanAnfMonicaJob.java
@@ -0,0 +1,21 @@
+package headFirsJava.chapter15sockets.threads;
+
+/**
+ * Created by User on 21.09.2017.
+ */
+class BankAccout {
+    private int balance = 100;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withdraw (int amount) {
+        balance = balance - amount;
+    }
+}
+
+public class RyanAnfMonicaJob {
+    
+
+}
