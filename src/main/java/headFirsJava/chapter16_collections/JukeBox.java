5e803bc7bcf9312ae1ff344ddafd328b5cbd2061
Vladyslove <od.vladyslav@gmail.com>
2017-09-27 01:20:28 +0300

    Method go starts loading the file and then prints the contents of the songList ArrayList.

--- src/main/java/headFirsJava/chapter16_collections/JukeBox.java
+++ src/main/java/headFirsJava/chapter16_collections/JukeBox.java
@@ -8,4 +8,18 @@ import java.util.ArrayList;
 public class JukeBox {
 
     ArrayList<String> songList = new ArrayList<String>();
+
+    public static void main(String[] args) {
+        new JukeBox().go();
+
+    }
+
+    private void go() {
+        getSongs();
+        System.out.println(songList);
+    }
+
+    private void getSongs() {
+
+    }
 }
