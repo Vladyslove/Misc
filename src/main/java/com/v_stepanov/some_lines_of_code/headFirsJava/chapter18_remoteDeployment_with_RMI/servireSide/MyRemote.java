ab7be7d68dea3edd49a053f81ebef92be15a3f1b
Vladyslove <od.vladyslav@gmail.com>
2017-10-02 23:45:12 +0300

    Started manupulation with ServerSide of remote deployment. 1- interface MUST extend Remote 2- all methods must declare a RemoteEx

---
+++ src/main/java/com/v_stepanov/some_lines_of_code/headFirsJava/chapter18_remoteDeployment_with_RMI/servireSide/MyRemote.java
@@ -0,0 +1,12 @@
+package com.v_stepanov.some_lines_of_code.headFirsJava.chapter18_remoteDeployment_with_RMI.servireSide;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Created by User on 02.10.2017.
+ */
+
+public interface MyRemote extends Remote {
+    public String sayHello() throws RemoteException;
+}
---
+++ src/main/java/com/v_stepanov/some_lines_of_code/headFirsJava/chapter18_remoteDeployment_with_RMI/servireSide/MyRemoteImpl.java
@@ -0,0 +1,5 @@
+package com.v_stepanov.some_lines_of_code.headFirsJava.chapter18_remoteDeployment_with_RMI.servireSide;
+
+public class MyRemoteImpl {
+
+}
