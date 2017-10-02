5b2239e24ed279d475e9cc99bb1a2c3c6d4d0338
Vladyslove <od.vladyslav@gmail.com>
2017-10-02 19:27:14 +0300

    Started learning of remote Deployment. Added simple example of with object on the same heap, run by the same JVM

---
+++ src/main/java/com/v_stepanov/some_lines_of_code/headFirsJava/chapter18_remoteDeployment_with_RMI/Foo.java
@@ -0,0 +1,26 @@
+package com.v_stepanov.some_lines_of_code.headFirsJava.chapter18_remoteDeployment_with_RMI;
+
+/**
+ * Created by User on 02.10.2017.
+ */
+public class Foo {
+    void go() {
+        Bar b = new Bar();
+        b.doStuff();
+
+
+    }
+
+    public static void main(String[] args) {
+        Foo f = new Foo();
+        f.go();
+    }
+    
+}
+
+class Bar {
+    void doStuff() {
+        // Method is doing smth
+    }
+
+}
