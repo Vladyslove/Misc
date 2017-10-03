cfa9b48ab4a5e44c4205643045916715a2755831
Vladyslove <od.vladyslav@gmail.com>
2017-10-03 19:07:27 +0300

    Added content of method go. Casted object of Naming class. This object consist of: IP address/hostname and name used to bind/rebind the service

--- src/main/java/com/v_stepanov/some_lines_of_code/headFirsJava/chapter18_remoteDeployment_with_RMI/servireSide/MyRemoteClient.java
+++ src/main/java/com/v_stepanov/some_lines_of_code/headFirsJava/chapter18_remoteDeployment_with_RMI/servireSide/MyRemoteClient.java
@@ -1,10 +1,25 @@
 package com.v_stepanov.some_lines_of_code.headFirsJava.chapter18_remoteDeployment_with_RMI.servireSide;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 public class MyRemoteClient {
     public static void main(String[] args) {
         new MyRemoteClient().go();
     }
 
     private void go() {
+        try {
+            MyRemote service = (MyRemote) Naming.lookup("rmi:/127.0.0.1/Remote Hello");
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
     }
 }
