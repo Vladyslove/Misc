5896aa525b5d30f5bcd325cf782e958af9432872
Vladyslove <od.vladyslav@gmail.com>
2017-10-03 18:59:58 +0300

    Started Client side of RMI.Added instance an method go()

---
+++ src/main/java/com/v_stepanov/some_lines_of_code/headFirsJava/chapter18_remoteDeployment_with_RMI/servireSide/MyRemoteClient.java
@@ -0,0 +1,10 @@
+package com.v_stepanov.some_lines_of_code.headFirsJava.chapter18_remoteDeployment_with_RMI.servireSide;
+
+public class MyRemoteClient {
+    public static void main(String[] args) {
+        new MyRemoteClient().go();
+    }
+
+    private void go() {
+    }
+}
