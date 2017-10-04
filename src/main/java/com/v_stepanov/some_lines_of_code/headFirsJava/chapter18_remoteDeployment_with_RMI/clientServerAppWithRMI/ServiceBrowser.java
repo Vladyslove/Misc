ea9e9efed352c2c71ec10030eff78e701a89721d
Vladyslove <od.vladyslav@gmail.com>
2017-10-05 00:22:30 +0300

    added GUI

--- src/main/java/com/v_stepanov/some_lines_of_code/headFirsJava/chapter18_remoteDeployment_with_RMI/clientServerAppWithRMI/ServiceBrowser.java
+++ src/main/java/com/v_stepanov/some_lines_of_code/headFirsJava/chapter18_remoteDeployment_with_RMI/clientServerAppWithRMI/ServiceBrowser.java
@@ -2,6 +2,7 @@ package com.v_stepanov.some_lines_of_code.headFirsJava.chapter18_remoteDeploymen
 
 import javax.swing.*;
 import java.awt.*;
+import java.rmi.RemoteException;
 
 /**
  * Created by User on 04.10.2017.
@@ -20,5 +21,25 @@ public class ServiceBrowser {
         Object[] services = getServicesList();
 
         serviceList = new JComboBox(services);
+
+        frame.getContentPane().add(BorderLayout.NORTH, serviceList);
+
+        serviceList.addActionListener(new MyListener());
+
+        frame.setSize(500,500);
+        frame.setVisible(true);
+    }
+
+    void loadService(Object serviceSelection) {
+        try {
+            Service svc = server.getService(serviceSelection);
+
+            mainPanel.removeAll();
+            mainPanel.add(svc.getGuiPanel());
+            mainPanel.validate();
+            mainPanel.repaint();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
