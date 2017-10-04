366daa9138d55cb4bce6e56d8a9eec3dce754b47
Vladyslove <od.vladyslav@gmail.com>
2017-10-04 16:08:27 +0300

    When the constructor is called, initialize the actual universal sevices(DiceService etc). Make the services(the actual service objects) and put them into the HashMap, with the HashMap, with a String name(for the 'key')

--- src/main/java/com/v_stepanov/some_lines_of_code/headFirsJava/chapter18_remoteDeployment_with_RMI/clientServerAppWithRMI/ServiceServerImpl.java
+++ src/main/java/com/v_stepanov/some_lines_of_code/headFirsJava/chapter18_remoteDeployment_with_RMI/clientServerAppWithRMI/ServiceServerImpl.java
@@ -2,13 +2,23 @@ package com.v_stepanov.some_lines_of_code.headFirsJava.chapter18_remoteDeploymen
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
 
 /**
  * Created by User on 04.10.2017.
  */
 public class ServiceServerImpl extends UnicastRemoteObject implements ServiceServer {
+    HashMap serviceList;
 
     public ServiceServerImpl() throws RemoteException {
+        setUpServices();
+    }
+
+    private void setUpServices() {
+        serviceList = new HashMap();
+        serviceList.put("Dice Rolling Service", new DiceService());
+        serviceList.put("Day of the Week Service", new DayOfTheWeekService());
+        serviceList.put("Vusual Music Servie", new MiniMusicService());
     }
 
     @Override
