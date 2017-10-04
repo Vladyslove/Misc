77250b319845ff55fbed4bc79b5d8cbd1c94f178
Vladyslove <od.vladyslav@gmail.com>
2017-10-04 16:01:30 +0300

    The service will be stored in HashMap collection. Instead of puttind ONE object in teh collection you put 2 == key object(like a String) and a Value object(whatever you want)

--- src/main/java/com/v_stepanov/some_lines_of_code/headFirsJava/chapter18_remoteDeployment_with_RMI/clientServerAppWithRMI/ServiceServerImpl.java
+++ src/main/java/com/v_stepanov/some_lines_of_code/headFirsJava/chapter18_remoteDeployment_with_RMI/clientServerAppWithRMI/ServiceServerImpl.java
@@ -1,9 +1,23 @@
 package com.v_stepanov.some_lines_of_code.headFirsJava.chapter18_remoteDeployment_with_RMI.clientServerAppWithRMI;
 
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
 /**
  * Created by User on 04.10.2017.
  */
-public class ServiceServerImpl {
+public class ServiceServerImpl extends UnicastRemoteObject implements ServiceServer {
 
+    public ServiceServerImpl() throws RemoteException {
+    }
 
+    @Override
+    public Object[] getServiceList() throws RemoteException {
+        return new Object[0];
+    }
+
+    @Override
+    public Service getService(Object serviceKey) throws RemoteException {
+        return null;
+    }
 }
