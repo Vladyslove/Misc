4661a316098fca6594eb8367ae4abb597879922c
Vladyslove <od.vladyslav@gmail.com>
2017-10-04 15:43:17 +0300

    Started development of simple client-sever programm by using RMI

---
+++ src/main/java/com/v_stepanov/some_lines_of_code/headFirsJava/chapter18_remoteDeployment_with_RMI/clientServerAppWithRMI/ServiceServerImpl.java
@@ -0,0 +1,9 @@
+package com.v_stepanov.some_lines_of_code.headFirsJava.chapter18_remoteDeployment_with_RMI.clientServerAppWithRMI;
+
+/**
+ * Created by User on 04.10.2017.
+ */
+public class ServiceServerImpl {
+
+
+}
