b161ba4d280fe36480985f32649270cd47d29fee
Vladyslove <od.vladyslav@gmail.com>
2017-11-11 00:19:07 +0200

    Added dynamic possibility to change behaviour of the ducks

--- src/main/java/com/v_stepanov/some_lines_of_code/designpatterns/stretegy/MiniDuckSimulator.java
+++ src/main/java/com/v_stepanov/some_lines_of_code/designpatterns/stretegy/MiniDuckSimulator.java
@@ -1,12 +1,24 @@
 package com.v_stepanov.some_lines_of_code.designpatterns.stretegy;
 
+import com.v_stepanov.some_lines_of_code.designpatterns.stretegy.behavior.implementation.Flying.FlyRocketPowered;
 import com.v_stepanov.some_lines_of_code.designpatterns.stretegy.ducks.MallardDuck;
+import com.v_stepanov.some_lines_of_code.designpatterns.stretegy.ducks.ModelDuck;
 
 public class MiniDuckSimulator {
     public static void main(String[] args) {
         Duck mallard = new MallardDuck();
-
         mallard.perfromQuack();
         mallard.perfromFly();
+
+       /* // My test code
+       FlyBehavior flyBehavior = new FlyWIthWings();
+        mallard.flyBehavior.fly();
+        mallard.setFlyBehavior(new FlyNoWay());
+        mallard.perfromFly();*/
+
+        Duck modelDuck = new ModelDuck();
+        modelDuck.perfromFly();
+        modelDuck.setFlyBehavior(new FlyRocketPowered());
+        modelDuck.perfromFly();
     }
 }
