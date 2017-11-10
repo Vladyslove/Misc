9314bcde301ef932ea858d904cbdd10633cb8bc2
Vladyslove <od.vladyslav@gmail.com>
2017-11-10 17:28:14 +0200

    nothing special, just testing

--- src/main/java/com/v_stepanov/some_lines_of_code/designpatterns/stretegy/Duck.java
+++ src/main/java/com/v_stepanov/some_lines_of_code/designpatterns/stretegy/Duck.java
@@ -1,6 +1,16 @@
 package com.v_stepanov.some_lines_of_code.designpatterns.stretegy;
 
+import com.v_stepanov.some_lines_of_code.designpatterns.stretegy.behavior.FlyBehavior;
+import com.v_stepanov.some_lines_of_code.designpatterns.stretegy.behavior.QuackBehavior;
+
 public class Duck {
+
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+    void perfromQuack() {
+        quackBehavior.quack();
+    }
+    void perfromFly() {}
     void swim(){    }
     protected void display(){    }
 
