de4f19e7d53fdfded71f6d8c203c4f9a32500ac9
Vladyslove <od.vladyslav@gmail.com>
2017-11-11 20:53:45 +0200

    Design pattern Strategy is totally observed

--- src/main/java/com/v_stepanov/some_lines_of_code/designpatterns/stretegy/ducks/RedheadDuck.java
+++ src/main/java/com/v_stepanov/some_lines_of_code/designpatterns/stretegy/ducks/RedheadDuck.java
@@ -1,22 +1,10 @@
 package com.v_stepanov.some_lines_of_code.designpatterns.stretegy.ducks;
 
 import com.v_stepanov.some_lines_of_code.designpatterns.stretegy.Duck;
-import com.v_stepanov.some_lines_of_code.designpatterns.stretegy.behavior.FlyBehavior;
-import com.v_stepanov.some_lines_of_code.designpatterns.stretegy.behavior.QuackBehavior;
 
-public class RedheadDuck extends Duck implements FlyBehavior, QuackBehavior {
+public class RedheadDuck extends Duck {
     @Override
     public void display() {
-//        super.display();
-    }
-
-    @Override
-    public void fly() {
-
-    }
-
-    @Override
-    public void quack() {
 
     }
 }
--- src/main/java/com/v_stepanov/some_lines_of_code/designpatterns/stretegy/ducks/RubberDuck.java
+++ src/main/java/com/v_stepanov/some_lines_of_code/designpatterns/stretegy/ducks/RubberDuck.java
@@ -1,13 +1,8 @@
 package com.v_stepanov.some_lines_of_code.designpatterns.stretegy.ducks;
 
 import com.v_stepanov.some_lines_of_code.designpatterns.stretegy.Duck;
-import com.v_stepanov.some_lines_of_code.designpatterns.stretegy.behavior.QuackBehavior;
 
-public class RubberDuck extends Duck implements QuackBehavior {
-
-    @Override
-    public void quack() {
-    }
+public class RubberDuck extends Duck {
 
     @Override
     public void display() {
